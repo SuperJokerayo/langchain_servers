a8ee09ab86f1781fc9cc359e1e3a349660c4f576
SuperJokerayo <1072843554@qq.com>
2023-09-07 23:32:45 +0800

    add pdf loader

---
+++ servers/document_qa/loaders/pdf_loader.py
@@ -0,0 +1,39 @@
+import fitz
+import numpy as np
+
+from rapidocr_onnxruntime import RapidOCR
+from unstructured.partition.text import partition_text
+
+from langchain.document_loaders.unstructured import UnstructuredFileLoader
+
+class PDFLoader(UnstructuredFileLoader):
+    def __init__(self, pdf_path):
+        super().__init__(file_path = pdf_path)
+        self.pdf_path = pdf_path
+
+    # extractor text and imgs in pdf
+    def _get_elements(self):
+        doc = fitz.open(self.pdf_path)
+        response = ""
+        ocr = RapidOCR()
+
+        for page in doc:
+            text = page.get_text()
+            response += text + "\n"
+
+            imgs = page.get_images()
+
+            for img in imgs:
+                pix = fitz.Pixmap(doc, img[0])
+                img_array = np.frombuffer(pix.samples, dtype = np.uint8).reshape(pix.height, pix.width, -1)
+                result, _ = ocr(img_array)
+
+                if result:
+                    ocr_result = [line[1] for line in result]
+                    response += "\n".join(ocr_result)
+        return partition_text(text = response, **self.unstructured_kwargs)
+
+if __name__ == "__main__":
+    loader = PDFLoader(pdf_path = "../../../assets/test.pdf")
+    text = loader.load()
+    print(text)
